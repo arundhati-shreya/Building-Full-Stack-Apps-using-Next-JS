bc1a08cbd7cc8aac616f5cf4ec91e8995246cb2a
arundhati-shreya <arundhati0426@gmail.com>
2024-02-13 21:50:33 +0530

    SEO Friendly

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,25 +1,33 @@
-import { MongoClient } from 'mongodb';
-import { ObjectId } from 'mongodb';
-import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { MongoClient } from "mongodb";
+import { ObjectId } from "mongodb";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { Fragment } from "react";
+import Head from "next/head";
 
 function MeetupDetails(props) {
   return (
-    <MeetupDetail
-      image={props.meetupData.image}
-      title={props.meetupData.title}
-      address={props.meetupData.address}
-      description={props.meetupData.description}
-    />
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description}/>
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
   );
 }
 
 export async function getStaticPaths() {
   const client = await MongoClient.connect(
-    'mongodb+srv://new_user_31:aruhh01@cluster0.b6xhdxb.mongodb.net/?retryWrites=true&w=majority'
+    "mongodb+srv://new_user_31:aruhh01@cluster0.b6xhdxb.mongodb.net/?retryWrites=true&w=majority"
   );
   const db = client.db();
 
-  const meetupsCollection = db.collection('meetups');
+  const meetupsCollection = db.collection("meetups");
 
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
 
@@ -39,11 +47,11 @@ export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
   const client = await MongoClient.connect(
-    'mongodb+srv://new_user_31:aruhh01@cluster0.b6xhdxb.mongodb.net/?retryWrites=true&w=majority'
+    "mongodb+srv://new_user_31:aruhh01@cluster0.b6xhdxb.mongodb.net/?retryWrites=true&w=majority"
   );
   const db = client.db();
 
-  const meetupsCollection = db.collection('meetups');
+  const meetupsCollection = db.collection("meetups");
 
   const selectedMeetup = await meetupsCollection.findOne({
     _id: new ObjectId(meetupId),
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,28 +1,38 @@
 // our-domain.com/new-meetup
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import { useRouter } from "next/router";
 
-import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
   const router = useRouter();
 
   async function addMeetupHandler(enteredMeetupData) {
-    const response = await fetch('/api/new-meetup',{
-      method:'POST',
-      body:JSON.stringify(enteredMeetupData),
-      headers:{
-        'Content-Type':'application/json'
-      }
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     const data = await response.json();
     console.log(data);
-    
-    router.push('/')
+
+    router.push("/");
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  return(
+    <>
+    <Head>
+      <title>Add a New Meetup</title>
+      <meta 
+      name="description"
+      content="Add your own meetups and create amazing networking oppurtunities "/>
+    </Head>
+     <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+);
 }
-
 
 export default NewMeetupPage;
